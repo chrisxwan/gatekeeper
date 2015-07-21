50dc61836905699aa792658f9b8ee0b81ce870b5
Janita Chalam <janitachalam@Janitas-MacBook-Pro.local>
2015-07-21 12:56:14 -0400

    update UI

--- popup.js
+++ popup.js
@@ -2,6 +2,21 @@ $(function() {
 	var hashtags = {}; //add trending hashtags from twitter
 	var keywords = []; //add user input keywords
 	$(document).ready(function() {
+	var populateUserList = function() {
+		chrome.storage.sync.get("userBlacklist", function (result) {
+			console.log('hi');
+			var userList = result.userBlacklist === undefined ? [] : result.userBlacklist;
+			if(userList.length === 0) {
+				var htmlString = '<div class="row-fluid blocked-keyword"><div class="col-xs-14 center">No blocked keywords. Add a new one!</div></div>';
+				$('#user-blacklist').append(htmlString);
+			} else {
+				for(i = 0; i < userList.length; i++) {
+					var htmlString = '<div class="row-fluid blocked-keyword" id="shift-right"><div class="col-xs-5">' + userList[i] + '</div><div class="col-xs-3"><button class="btn btn-xs" id="delete">Delete</button></div></div>';
+					$('#user-blacklist').append(htmlString);
+				}
+			}
+		});
+	};
 		populateUserList();
 		$(".text-input").keydown(function(event) {
 			if (event.keyCode == 13) {
@@ -72,30 +87,16 @@ $(function() {
 				console.log('value deleted');
 			});
 		});
-	}
+	};
 
-	var populateUserList = function() {
-		chrome.storage.sync.get("userBlacklist", function (result) {
-			console.log('hi');
-			var userList = result.userBlacklist === undefined ? [] : result.userBlacklist;
-			if(userList.length === 0) {
-				var htmlString = '<div class="row-fluid blocked-keyword"><div class="col-xs-14 center">No blocked keywords. Add a new one!</div></div>';
-				$('#user-blacklist').append(htmlString);
-			} else {
-				for(i = 0; i < userList.length; i++) {
-					var htmlString = '<div class="row-fluid blocked-keyword" id="shift-right"><div class="col-xs-5">' + userList[i] + '</div><div class="col-xs-3"><button class="btn btn-xs" id="delete">Delete</button></div></div>';
-					$('#user-blacklist').append(htmlString);
-				}
-			}
-		});
-	}
+
 
 	chrome.storage.onChanged.addListener(function(changes, namespace) {
 		if(changes.userBlacklist) {
 			$('.blocked-keyword').remove();
 			populateUserList();
 		}
-	})
+	});
 
 	var submit = function(elem) {
 		var val = $(elem).val();
@@ -116,4 +117,5 @@ $(function() {
 			}
 		});
 	};
+});
 });
