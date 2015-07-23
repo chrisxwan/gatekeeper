4b1693894acbb7c558b6e186b8c4aef8ae5004f4
Christopher Wan <christopher.wan@yale.edu>
2015-07-22 21:08:13 -0400

    add circle.yml and generate json file

--- js/twitter.js
+++ js/twitter.js
@@ -13,7 +13,16 @@ var success = function (data) {
 var twitter = new Twitter(secrets.twitter);
 
 twitter.getCustomApiCall('/trends/place.json', { id: 23424977, exclude: 'hashtags' }, error, function (data) {
-	console.log('Data [%s]', data);
-	var path = 
-})
+	var trendsRaw = JSON.parse(data);
+	fs.writeFile("../trends.json", data);
+	// var twitterTrends = [];
+
+	// for(i = 0; i < trendsRaw.length; i++) {
+	// 	var trend = trendsRaw[i].name;
+	// 	twitterTrends.push(trend);
+	// 	fs.writeFile("trends.json", trend, "a");
+	// }
+	// var path = "twitterTrends.txt";
+	// fs.write(path, "test", "w");
+});
 
